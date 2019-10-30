12b3d91edddfa340146ccdbde5789b470f0e5076
viniciussso <vinicius.oliveira@ccc.ufcg.edu.br>
2019-10-29 21:27:41 -0300

    final

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const insert = {
   item:
     "INSERT INTO ITEM (IDENTIFICADOR, NUM_NOTA_FISCAL_ORDEM, NUMERO_NOTA_FISCAL, QUANTIDADE, PRECO_PRODUTO, DESCONTO) VALUES",
   realizaReclamacao:
-    "INSERT INTO REALIZA_MANUTENCAO (ID_RECLAMACAO, DATA_HORA, DESCRICAO, CODIGO_FILIAL, CPF_CLIENTE) VALUES"
+    "INSERT INTO REALIZA_RECLAMACAO (ID_RECLAMACAO, DATA_HORA, DESCRICAO, CODIGO_FILIAL, CPF_CLIENTE) VALUES"
 };
 
 let primaryKeys = {};
@@ -76,16 +76,16 @@ const getRandomDate = () => {
   )}','DD/MM/YYYY')`;
 };
 
-const clientes = () => {
-  let clientes = "\n\n----- CLIENTE -----\n\n";
+const cliente = () => {
+  let cliente = "\n\n------ CLIENTE ------\n\n";
 
-  primaryKeys.clientes = [];
+  primaryKeys.cliente = [];
 
   for (let idx = 0; idx < 20; idx++) {
     const cpf = getRandomCpf();
-    primaryKeys.clientes.push(cpf);
+    primaryKeys.cliente.push(cpf);
 
-    clientes += `${
+    cliente += `${
       insert.cliente
     } ('${cpf}', 'Cliente 0${idx}', 'cliente0${idx}@gmail.com', ${getRandomInt(
       0,
@@ -99,22 +99,22 @@ const clientes = () => {
     )}', 'Bairro ${getRandomInt(10, 100)}');\n`;
   }
 
-  return clientes;
+  return cliente;
 };
 
-const telefonesClientes = () => {
-  let telefonesClientes = "\n\n----- TELEFONE_CLIENTE -----\n\n";
+const telefoneCliente = () => {
+  let telefoneCliente = "\n\n------ TELEFONE_CLIENTE ------\n\n";
 
   for (let idx = 0; idx < 50; idx++) {
-    telefonesClientes += `${insert.telefoneCliente} ('${getElement(
-      primaryKeys.clientes
+    telefoneCliente += `${insert.telefoneCliente} ('${getElement(
+      primaryKeys.cliente
     )}', '${getRandomTelefone()}');\n`;
   }
 
-  return telefonesClientes;
+  return telefoneCliente;
 };
 
-const funcionarios = () => {
+const funcionario = () => {
   const funcao = [
     "Caixa",
     "Empacotador",
@@ -124,14 +124,14 @@ const funcionarios = () => {
     "Segurança"
   ];
 
-  let funcionarios = "\n\n----- FUNCIONARIO -----\n\n";
+  let funcionario = "\n\n------ FUNCIONARIO ------\n\n";
 
-  primaryKeys.funcionarios = [];
+  primaryKeys.funcionario = [];
 
   for (let idx = 0; idx < 15; idx++) {
-    primaryKeys.funcionarios.push(idx);
+    primaryKeys.funcionario.push(idx);
 
-    funcionarios += `${
+    funcionario += `${
       insert.funcionario
     } (${idx}, '${getRandomCpf()}', '${getRandomInt(
       100000,
@@ -140,94 +140,321 @@ const funcionarios = () => {
       1000,
       10000
     )}, '${getElement(funcao)}', ${
-      idx ? getElement(primaryKeys.funcionarios) : null
+      idx ? getElement(primaryKeys.funcionario) : null
     });\n`;
   }
 
-  return funcionarios;
+  return funcionario;
 };
 
-const telefonesFuncionarios = () => {
-  let telefonesFuncionarios = "\n\n----- TELEFONE_FUNCIONARIO -----\n\n";
+const telefoneFuncionario = () => {
+  let telefoneFuncionario = "\n\n------ TELEFONE_FUNCIONARIO ------\n\n";
 
   for (let idx = 0; idx < 30; idx++) {
-    telefonesFuncionarios += `${insert.telefoneFuncionario} (${getElement(
-      primaryKeys.funcionarios
+    telefoneFuncionario += `${insert.telefoneFuncionario} (${getElement(
+      primaryKeys.funcionario
     )}, '${getRandomTelefone()}');\n`;
   }
 
-  return telefonesFuncionarios;
+  return telefoneFuncionario;
 };
 
-const dependentes = () => {
-  let dependentes = "\n\n----- DEPENDENTE ------\n\n";
+const dependente = () => {
+  let dependente = "\n\n------ DEPENDENTE ------\n\n";
 
   for (let idx = 0; idx < 40; idx++) {
-    dependentes += `${
+    dependente += `${
       insert.dependente
-    } ('${getRandomCpf()}', ${getRandomDate()}, 'dependente 0${idx}', ${getElement(
-      primaryKeys.funcionarios
+    } ('${getRandomCpf()}', ${getRandomDate()}, 'Dependente 0${idx}', ${getElement(
+      primaryKeys.funcionario
     )});\n`;
   }
 
-  return dependentes;
+  return dependente;
 };
 
-const filiais = () => {
-  let filiais = "\n\n----- FILIAL ------\n\n";
+const filial = () => {
+  let filial = "\n\n------ FILIAL ------\n\n";
 
-  primaryKeys.filiais = [];
+  primaryKeys.filial = [];
 
   for (let idx = 0; idx < 15; idx++) {
-    primaryKeys.filiais.push(idx);
+    primaryKeys.filial.push(idx);
 
-    filiais += `${insert.filial} (${idx}, 'Filial ${idx}', 'Rua ${getRandomInt(
+    filial += `${insert.filial} (${idx}, 'Filial ${idx}', 'Rua ${getRandomInt(
       10,
       100
     )}', '${getRandomTelefone()}');\n`;
   }
 
-  return filiais;
+  return filial;
 };
 
-const marcas = () => {
-  let marcas = "\n\n----- MARCA ------\n\n";
+const marca = () => {
+  let marca = "\n\n------ MARCA ------\n\n";
 
-  primaryKeys.marcas = [];
+  primaryKeys.marca = [];
 
   for (let idx = 0; idx < 25; idx++) {
-    primaryKeys.marcas.push(idx);
+    primaryKeys.marca.push(idx);
 
-    marcas += `${insert.marca} (${idx}, 'Marca ${idx}');\n`;
+    marca += `${insert.marca} (${idx}, 'Marca ${idx}');\n`;
   }
 
-  return marcas;
+  return marca;
 };
 
-const categorias = () => {
-  let categorias = "\n\n----- CATEGORIA ------\n\n";
+const categoria = () => {
+  let categoria = "\n\n------ CATEGORIA ------\n\n";
 
-  primaryKeys.categorias = [];
+  primaryKeys.categoria = [];
 
   for (let idx = 0; idx < 20; idx++) {
-    primaryKeys.categorias.push(idx);
+    primaryKeys.categoria.push(idx);
 
-    categorias += `${insert.categoria} (${idx}, 'Categoria ${idx}');\n`;
+    categoria += `${insert.categoria} (${idx}, 'Categoria ${idx}');\n`;
   }
 
-  return categorias;
+  return categoria;
+};
+
+const produto = () => {
+  let produto = "\n\n------ PRODUTO ------\n\n";
+
+  primaryKeys.produto = [];
+
+  for (let idx = 0; idx < 100; idx++) {
+    primaryKeys.produto.push(idx);
+
+    produto += `${
+      insert.produto
+    } (${idx}, 'Produto 0${idx}', 'Descrição do Produto 0${idx}', '${getRandomInt(
+      1,
+      9999
+    )}', ${getElement(primaryKeys.filial)}, ${getRandomInt(
+      1,
+      1000
+    )}, ${getRandomInt(1, 30)}, ${getRandomInt(
+      30,
+      100
+    )}, ${getRandomDate()}, ${getRandomDate()}, ${getElement(
+      primaryKeys.marca
+    )}, ${getElement(primaryKeys.categoria)});\n`;
+  }
+
+  return produto;
+};
+
+const caixa = () => {
+  let caixa = "\n\n------ CATEGORIA ------\n\n";
+
+  primaryKeys.caixa = [];
+
+  for (let idx = 0; idx < 10; idx++) {
+    primaryKeys.caixa.push(idx);
+
+    caixa += `${insert.caixa} (${idx}, ${getElement(primaryKeys.filial)});\n`;
+  }
+
+  return caixa;
+};
+
+const equipamento = () => {
+  let equipamento = "\n\n------ EQUIPAMENTO ------\n\n";
+
+  primaryKeys.equipamento = [];
+
+  for (let idx = 0; idx < 20; idx++) {
+    primaryKeys.equipamento.push(idx);
+
+    equipamento += `${
+      insert.equipamento
+    } (${idx}, 'Descrição do Equipamento ${idx}', ${getElement(
+      primaryKeys.caixa
+    )});\n`;
+  }
+
+  return equipamento;
+};
+
+const realizaManutencao = () => {
+  let realizaManutencao = "\n\n------ REALIZA_MANUTENCAO ------\n\n";
+
+  primaryKeys.realizaManutencao = [];
+
+  for (let idx = 0; idx < 30; idx++) {
+    primaryKeys.realizaManutencao.push(idx);
+
+    realizaManutencao += `${insert.realizaManutencao} (${idx}, ${getElement(
+      primaryKeys.equipamento
+    )}, ${getElement(primaryKeys.caixa)}, ${getElement(
+      primaryKeys.funcionario
+    )}, ${getRandomInt(10000000000, 10999999999)}, ${getRandomInt(
+      10,
+      200
+    )});\n`;
+  }
+
+  return realizaManutencao;
+};
+
+const fornecedor = () => {
+  let fornecedor = "\n\n------ FORNECEDOR ------\n\n";
+
+  primaryKeys.fornecedor = [];
+
+  for (let idx = 0; idx < 10; idx++) {
+    const cnpj = getRandomCpf();
+    primaryKeys.fornecedor.push(cnpj);
+
+    fornecedor += `${
+      insert.fornecedor
+    } ('${cnpj}', 'Fornecedor ${idx}', 'Rua 0${idx}', 'fornecedor${idx}@gmail.com', ${getElement(
+      primaryKeys.categoria
+    )});\n`;
+  }
+
+  return fornecedor;
+};
+
+let cnpjSolicitacao = [];
+
+const solicitacao = () => {
+  let solicitacao = "\n\n------ SOLICITACAO ------\n\n";
+
+  primaryKeys.solicitacao = [];
+
+  for (let idx = 0; idx < 40; idx++) {
+    primaryKeys.solicitacao.push(idx);
+    const cnpjFornecedor = getElement(primaryKeys.fornecedor);
+    cnpjSolicitacao.push(cnpjFornecedor);
+
+    solicitacao += `${
+      insert.solicitacao
+    } (${idx}, ${getRandomDate()}, ${getRandomDate()}, ${getRandomDate()}, ${getRandomInt(
+      1000,
+      10000
+    )}, ${getRandomDate()}, ${getElement(
+      primaryKeys.filial
+    )}, '${cnpjFornecedor}');\n`;
+  }
+
+  return solicitacao;
+};
+
+const notaFiscal = () => {
+  let notaFiscal = "\n\n------ NOTA_FISCAL ------\n\n";
+
+  cnpjSolicitacao = cnpjSolicitacao.reverse();
+
+  primaryKeys.notaFiscal = [];
+
+  for (let idx = 0; idx < 40; idx++) {
+    primaryKeys.notaFiscal.push(idx);
+
+    notaFiscal += `${
+      insert.notaFiscal
+    } (${idx}, '${cnpjSolicitacao.pop()}', ${getRandomInt(
+      10,
+      1000
+    )}, ${getRandomDate()}, ${idx});\n`;
+  }
+
+  return notaFiscal;
+};
+
+const telefoneFornecedor = () => {
+  let telefoneFornecedor = "\n\n------ TELEFONE_FORNECEDOR ------\n\n";
+
+  for (let idx = 0; idx < 20; idx++) {
+    telefoneFornecedor += `${
+      insert.telefoneFornecedor
+    } ('${getRandomTelefone()}', '${getElement(primaryKeys.fornecedor)}');\n`;
+  }
+
+  return telefoneFornecedor;
+};
+
+const ordemCompra = () => {
+  let ordemCompra = "\n\n------ ORDEM_COMPRA ------\n\n";
+
+  primaryKeys.ordemCompra = [];
+
+  for (let idx = 0; idx < 50; idx++) {
+    primaryKeys.ordemCompra.push(idx);
+
+    ordemCompra += `${insert.ordemCompra} (${idx}, ${getRandomInt(
+      10000000000,
+      10999999999
+    )}, '${getElement(primaryKeys.cliente)}', ${getElement(
+      primaryKeys.filial
+    )}, ${getElement(primaryKeys.funcionario)}, ${getElement(
+      primaryKeys.caixa
+    )});\n`;
+  }
+
+  return ordemCompra;
+};
+
+const item = () => {
+  let item = "\n\n------ ITEM ------\n\n";
+
+  primaryKeys.item = [];
+
+  for (let idx = 0; idx < 50; idx++) {
+    primaryKeys.item.push(idx);
+
+    item += `${insert.item} (${idx}, ${getElement(
+      primaryKeys.ordemCompra
+    )}, ${getElement(primaryKeys.notaFiscal)}, ${getRandomInt(
+      1,
+      100
+    )}, ${getRandomInt(1, 100)}, ${getRandomInt(0, 10)});\n`;
+  }
+
+  return item;
+};
+
+const realizaReclamacao = () => {
+  let realizaReclamacao = "\n\n------ REALIZA_RECLAMACAO ------\n\n";
+
+  primaryKeys.realizaReclamacao = [];
+
+  for (let idx = 0; idx < 50; idx++) {
+    primaryKeys.realizaReclamacao.push(idx);
+
+    realizaReclamacao += `${insert.realizaReclamacao} (${idx}, ${getRandomInt(
+      10000000000,
+      10999999999
+    )}, 'Descrição da Reclamação ${idx}', ${getElement(
+      primaryKeys.filial
+    )}, '${getElement(primaryKeys.cliente)}');\n`;
+  }
+
+  return realizaReclamacao;
 };
 
 const generateInserts = () => {
   save(
-    clientes() +
-      telefonesClientes() +
-      funcionarios() +
-      telefonesFuncionarios() +
-      dependentes() +
-      filiais() +
-      marcas() +
-      categorias()
+    cliente() +
+      telefoneCliente() +
+      funcionario() +
+      telefoneFuncionario() +
+      dependente() +
+      filial() +
+      marca() +
+      categoria() +
+      produto() +
+      caixa() +
+      equipamento() +
+      realizaManutencao() +
+      fornecedor() +
+      solicitacao() +
+      notaFiscal() +
+      telefoneFornecedor() +
+      ordemCompra() +
+      item() +
+      realizaReclamacao()
   );
 };
 
